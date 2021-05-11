6a4629c2c516393b0f553d8768c6d2fa918c20d2
Saracen <SaracenOne@gmail.com>
2021-05-11 23:30:42 +0100

    Use simplified funcref syntax

--- network_logger.gd
+++ network_logger.gd
@@ -6,13 +6,10 @@ var message_error_func: FuncRef = FuncRef.new()
 
 
 func assign_printl_func(p_instance: Object, p_function: String) -> void:
-	message_printl_func.set_instance(p_instance)
-	message_printl_func.set_function(p_function)
-
+	message_printl_func = funcref(p_instance, p_function)
 
 func assign_error_func(p_instance: Object, p_function: String) -> void:
-	message_error_func.set_instance(p_instance)
-	message_error_func.set_function(p_function)
+	message_error_func = funcref(p_instance, p_function)
 
 
 func printl(p_text) -> void:
