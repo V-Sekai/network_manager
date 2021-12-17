2dfd7fa35d7125c3d9e19b3c33bfd845bd65edee
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2021-12-17 02:21:06 -0800

    Override enable and disable plugins.

--- network_manager_plugin.gd
+++ network_manager_plugin.gd
@@ -4,8 +4,12 @@ extends EditorPlugin
 var editor_interface: EditorInterface = null
 
 
-func _init():
+func _enable_plugin():
 	print("Initialising NetworkManager plugin")
+	editor_interface = get_editor_interface()
+
+	add_autoload_singleton("NetworkManager", "res://addons/network_manager/network_manager.gd")
+	add_autoload_singleton("NetworkLogger", "res://addons/network_manager/network_logger.gd")
 
 
 func _notification(p_notification: int):
@@ -18,13 +22,6 @@ func _get_plugin_name() -> String:
 	return "NetworkManager"
 
 
-func _enter_tree() -> void:
-	editor_interface = get_editor_interface()
-
-	add_autoload_singleton("NetworkManager", "res://addons/network_manager/network_manager.gd")
-	add_autoload_singleton("NetworkLogger", "res://addons/network_manager/network_logger.gd")
-
-
-func _exit_tree() -> void:
+func _disable_plugin() -> void:
 	remove_autoload_singleton("NetworkManager")
 	remove_autoload_singleton("NetworkLogger")
