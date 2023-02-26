ca5d13a8654457c2964a5a971eb92370f3600ed6
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-02-25 17:50:27 -0800

    Remove non player.

--- network_replication_manager.gd
+++ network_replication_manager.gd
@@ -178,8 +178,6 @@ func get_network_scene_id_from_path(p_path: String) -> int:
 		if network_scene_id == -1:
 			if (
 				path != "res://addons/vsk_entities/vsk_player.tscn"
-				and path != "res://addons/vsk_entities/vsk_interactable_prop.tscn"
-				and path != "res://addons/vsk_entities/vsk_test_entity.tscn"
 			):
 				push_error(
 					"SECURITY: " + str(self) + "@" + str(self.get_path()) + ": Checking for resource at " + str(path)
@@ -367,8 +365,6 @@ func get_scene_path_for_scene_id(p_scene_id: int) -> String:
 func get_packed_scene_for_path(p_path: String) -> PackedScene:
 	if (
 		p_path != "res://addons/vsk_entities/vsk_player.tscn"
-		and p_path != "res://addons/vsk_entities/vsk_interactable_prop.tscn"
-		and p_path != "res://addons/vsk_entities/vsk_test_entity.tscn"
 	):
 		push_error(
 			"SECURITY: " + str(self) + "@" + str(self.get_path()) + ": get_packed_scene_for_path at " + str(p_path)
